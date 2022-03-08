87fec7d0a044f400a0ed22fc4dfd0683b2318f3f
Tiago Rossi <tihdrossi03@gmail.com>
2022-03-08 17:09:56 -0300

    Form layout

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
     <nav className="container">
         <div className="dsmovie-nav-content">
             <h1>DSMovie</h1>
-            <a href="https://github.com/tihdfooxx02/dsmove" target="_blank" rel="noreferrer">
+            <a href="https://github.com/tihdfooxx02/dsmove/commits?author=TiagoRRossiG" target="_blank" rel="noreferrer">
                 <div className="dsmovie-contact-container">
                     <GithubIcon />
                     <p className="dsmovie-contact-link">/tihdfooxx02</p>
