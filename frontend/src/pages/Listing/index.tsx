56d5cb2f0bec8c43b6302a7981d6b1fe2fafee7c
Tiago Rossi <tihdrossi03@gmail.com>
2022-03-09 14:10:45 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,32 @@
+import MovieCard from "components/MovieCard";
 import Pagnation from "components/Pagination";
 
 function Listing(){
 
     return (
+        <>
         <Pagnation />
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />     
+                </div> 
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />     
+                </div> 
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />     
+                </div> 
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />     
+                </div> 
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />     
+                </div> 
+            </div>
+        </div>
+
+        </>
     );
 }
 
