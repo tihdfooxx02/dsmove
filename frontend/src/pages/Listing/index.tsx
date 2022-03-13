1dd3f9d02a8d663472460814fbfacecca2323d2a
Tiago Rossi <tihdrossi01@gmail.com>
2022-03-13 07:48:49 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -28,9 +28,16 @@ function Listing(){
         });
     }, [pageNumber]);
 
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber)
+    }
+
+
+
     return (
         <>
-        <Pagnation />
+        <Pagnation page = {page} onChange={handlePageChange} />
+
         <div className="container">
             <div className="row">
                 {page.content.map(movie =>(
